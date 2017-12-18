9ee17b58c7b9bb5e8a5c1614e55200d72958fb26
unknown <jmcarroll95@gmail.com>
2017-12-18 10:13:39 -0500

    added basic html pages, and router.js that connects all the pages together

---
+++ router.js
@@ -0,0 +1,53 @@
+var http = require("http");
+var fs = require("fs");
+
+var PORT = 8080;
+
+var server = http.createServer(handleRequest);
+
+// Start our server
+server.listen(PORT, function() {
+  // Callback triggered when server is successfully listening. Hurray!
+  console.log("Server listening on: http://localhost:" + PORT);
+});
+
+// Create a function which handles incoming requests and sends responses
+function handleRequest(req, res) {
+
+  // Capture the url the request is made to
+  var path = req.url;
+
+  // Depending on the URL, display a different HTML file.
+  switch (path) {
+
+    case "/food":
+        return fs.readFile(__dirname + "/food.html", function(err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+        });
+
+    case "/movies":
+        return fs.readFile(__dirname + "/movies.html", function(err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+        });
+
+    case "/frameworks":
+        return fs.readFile(__dirname + "/frameworks.html", function(err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+        });
+
+    case "/test":
+        return fs.readFile(__dirname + "/test.html", function(err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+        });
+
+    default:
+        return fs.readFile(__dirname + "/index.html", function(err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+        });
+  }
+}
